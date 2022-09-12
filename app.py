547e0b11201c52ea0a03357ddeef24c48dcd06cf
Jun <junsoopark92@gmail.com>
2022-09-12 10:44:09 -0400

    convert an image of text

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import pytesseract
 from PIL import Image
 
+pytesseract.pytesseract.tesseract_cmd = r"/opt/homebrew/Cellar/tesseract/5.2.0/bin/tesseract"
 
+receipt1 = Image.open('receipt.jpg')
+res = pytesseract.image_to_string(receipt1)
 
+print(res)
